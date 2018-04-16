d83dd0822ed7555feb0226370a894a1976bc9c63
Maksim Shlomin <maksim.shlomin@iqoption.com>
2018-04-16 22:42:08 +0300

    exercise 09

---
+++ src/main/scala/com/scalaforcompatient/chapter_01/ex_09.scala
@@ -0,0 +1,8 @@
+package com.scalaforcompatient.chapter_01
+
+object ex_09 extends App {
+  val string: String = "string"
+  println(string(0))
+  println(string.last)
+  println(string(string.length - 1))
+}
